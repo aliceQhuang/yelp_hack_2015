e11ab147d5c56ec68306cd46c5c54518118d01b9
Brendan Zhao <crickets147@gmail.com>
2015-02-27 12:36:39 -0800

    LOOOOOOOOOOOOOL

--- javascripts/main.js
+++ javascripts/main.js
@@ -143,7 +143,24 @@ $(document).ready(function() {
         generateMoveMarkup(user.move);
     }
 
-    var alice = function() {
-
+    var blinkyblinky = function() {
+        var el = $('blink h6')[0]
+        if (el.style.visibility === 'visible') {
+            el.style.visibility = 'hidden';
+            el.style.color = getRandomColor()
+        } else {
+            el.style.visibility = 'visible';
+        }
     }
+
+    var getRandomColor = function() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    setInterval(blinkyblinky, 750);
 });
